67c35d0f0301aac1cdc00ff60e573713dc55f9b1
Douglas Paz <oi@douglaspaz.com>
2020-10-24 09:49:33 -0300

    update music-info component

--- src/components/music-info.js
+++ src/components/music-info.js
@@ -1,10 +1,32 @@
 import Vue from 'vue'
+import HasVideoInfo from './has-video-info'
+import utils from '../utils'
 
 export default Vue.component(
   'MusicInfo',
   {
+    extends: HasVideoInfo,
     template: `
-      <div />
-    `
+      <div>
+        <div v-if="musicInfo">
+          <p>{{ musicInfo.title }}</p>
+          <p>by <strong>{{ musicInfo.artist }}</strong></p>
+        </div>
+        <div v-else>
+          <p>Loading music info...</p>
+        </div>
+      </div>
+    `,
+    props: {
+      videoId: {
+        type: String,
+        required: true
+      }
+    },
+    computed: {
+      musicInfo () {
+        return utils.getMusicInfoFromYTMusicAppState(this.$videoStates.states[this.videoId])
+      }
+    }
   }
 )
