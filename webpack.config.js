7c57c8210d87ba56473405288fcc08bff71046ed
Douglas Paz <oi@douglaspaz.com>
2021-03-28 00:19:29 -0300

    feat: upgrade webpack to v5

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,74 @@
 const path = require('path')
+const webpack = require('webpack')
 const CopyPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  entry: {
-    background: './src/background.js',
-    popup: './src/popup.js',
-    content: './src/content.js'
-  },
+const common = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        'manifest.json'
-      ]
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'popup.html',
-      template: './src/popup.ejs',
-      title: 'Youtube Music DL - Popup',
-      chunks: ['popup']
-    })
-  ],
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
   }
 }
+
+module.exports = [
+  {
+    target: 'webworker',
+    entry: {
+      background: './src/background.js'
+    },
+    resolve: {
+      fallback: {
+        url: require.resolve('url/'),
+        timers: require.resolve('timers-browserify'),
+        stream: require.resolve('stream-browserify'),
+        https: require.resolve('https-browserify'),
+        http: require.resolve('stream-http'),
+        path: require.resolve('path-browserify'),
+        util: require.resolve('util/')
+      }
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer']
+      }),
+      new CopyPlugin({
+        patterns: [
+          'manifest.json'
+        ]
+      })
+    ],
+    performance: {
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000
+    },
+    ...common
+  },
+  {
+    target: 'web',
+    entry: {
+      popup: './src/popup.js'
+    },
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js'
+      }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'popup.html',
+        template: './src/popup.ejs',
+        title: 'Youtube Music DL - Popup',
+        chunks: ['popup']
+      })
+    ],
+    ...common
+  },
+  {
+    target: 'web',
+    entry: {
+      content: './src/content.js'
+    },
+    ...common
+  }
+]
